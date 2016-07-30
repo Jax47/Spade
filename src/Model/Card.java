82305649260d4bc8f16cdf92bcbbbb007bb0185a
Joshua Anderson <jscottanderson1994@gmail.com>
2016-07-30 19:06:01 -0400

    Added various methods to Card class and change constructor
    Renamed the val in the Card constructor to value for readabilty(and
    mentailty)
    
    	getValue: This method will return a card's value.
    
    	getSuit: This method return a card's suit
    
    	initalizeDeck: This method will create an arraylist of card object and
    	 shuffle the arraylist and then return the arraylist.
    
    	removeCard: This method remove a card from an arraylist using the
    	 Card_ID which consist of integer in a Pair object. It take an arraylist
    	 and Pair object as argument.
    
    	compareTo: This method compare card object using the card's suit and
    	 value. It return integer 1, -1 or 0.

--- src/Model/Card.java
+++ src/Model/Card.java
@@ -1,8 +1,9 @@
 package Model;
 
-import java.awt.Dimension;
-import java.util.Collection;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Iterator;
 
 import Util.Pair;
 
@@ -27,7 +28,7 @@ import Util.Pair;
  */
 public class Card implements Comparable<Card> {
 	
-	/*
+	/**
 	 * A enumeration of the suit value with integer from 4 to 1 assign
 	 * with Spade assign the highest value 4 and Heart assign the 
 	 * lowest value 1. 
@@ -48,7 +49,7 @@ public class Card implements Comparable<Card> {
 	
 	};
 	
-	/*
+	/**
 	 * An enumeration of the value shown on the card staring from 2
 	 * to 10 and including King, Queen, Jack and Ace. Each value will
 	 * have an integer assign to it as according to the rule of the 
@@ -101,22 +102,127 @@ public class Card implements Comparable<Card> {
 	
 	//The card's suit and value object
 	private final Suit suit;
-	private final Value val;
+	private final Value value;
 	
 	//The card unique id
 	private final Pair Card_ID;
 	
-	public Card(Suit suit, Value val){
+	public Card(Suit suit, Value value){
 		this.suit = suit;
-		this.val = val;
-		this.Card_ID = new Pair(this.suit.getVal(), this.val.getVal());
+		this.value = value;
+		this.Card_ID = new Pair(this.suit.getVal(), this.value.getVal());
+		
+	}
+	/**
+	 * A method to return the card's Suit value
+	 * 
+	 * @return The suit value of a card object.
+	 */
+	public Suit getSuit(){
+		return this.suit;
+	}
+	
+	/**
+	 * A method to get the card's value however 
+	 * for comparison of two card object you 
+	 * must append the method getVal() to this method
+	 * 
+	 * @return An Enum key of a card object.
+	 *  
+	 */
+	public Value getValue(){
+		return this.value;
+	}
+	
+	/**
+	 * 
+	 * A method that initialize a deck of card by
+	 * creating an arraylist of card object while also
+	 * shuffling it as well.
+	 * 
+	 * @return An arraylist of card object
+	 */
+	
+	public ArrayList<Card> initalizeDeck(){
+		ArrayList<Card> deck = new ArrayList<Card>();
+		/*
+		 * Loop through each enumeration starting with suit 
+		 * and then value to create all 52 cards object and
+		 * add it to the arraylist. 
+		 */
+		for(Suit eleS : EnumSet.allOf(Suit.class)){
+			for(Value eleV: EnumSet.allOf(Value.class)){
+				deck.add(new Card(eleS, eleV));
+			}
+		}
+		
+		//Shuffle the deck 
+		Collections.shuffle(deck);
+		return deck;}
+	
+	/**
+	 * A method that remove a specific card from the 
+	 * deck(an arraylist of card).
+	 * 
+	 * @param Deck The arraylist of card object.
+	 * @param target The targeted card to be remove from the arraylist.
+	 */
+	
+	public void removeCard(ArrayList<Card> Deck, Pair Target){
+		
+		//Iterate through each card in the deck while there is still a card in the deck
+		for(Iterator<Card> element = Deck.iterator(); element.hasNext();){
+			
+			//Set variable card to the next card in the deck
+			Card card = element.next();
+			
+			//check if the card is the target 
+			if(card.Card_ID.equals(Target)){
+				element.remove();
+			}
+		}
 		
 	}
 	
 	
+	/**
+	 * A method to compare the suit value and card value
+	 * 
+	 * @return
+	 * 		<b> 1</b>  :if the object being compared is less than the object <br>
+	 * 		<b>-1</b>  :if the object being compared is greater than the object <br> 
+	 * 		<b> 0</b>  :if the object being compared is equal to the object
+	 * 
+	 * 		 
+	 */
 	@Override
 	public int compareTo(Card o) {
-		// TODO Auto-generated method stub
+		// Check if there are null type
+		if(this.equals(null) || o.equals(null)){
+			System.err.println("Can not compare with a null type.");
+			return 0;
+		}
+		
+		// Check if the suit are the same
+		if(this.getSuit().equals(o.getSuit())){
+			
+			//check who has the highest value
+			if(this.getValue().getVal() > o.getValue().getVal()){
+				return 1;
+			}
+			//Check who has the lowest value
+			else if(this.getValue().getVal() < o.getValue().getVal()){
+				return -1;
+			}
+		}
+		//Check who has the highest suit
+		else if(this.getSuit().getVal() > o.getSuit().getVal()){
+			return 1;
+		}
+		//Check who has the lowest suit
+		else if(this.getSuit().getVal() < o.getSuit().getVal()){
+			return -1;
+		}
 		return 0;
 	}
 
